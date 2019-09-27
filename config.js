1a707fd62b1616b6964383c800fd722a0d7e2d47
Mangor1no <39774481+Mangor1no@users.noreply.github.com>
2019-09-27 17:30:22 +0700

    meh

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    crawlURL: 'https://quizlet.com/280976596/terms-definitions-800-flash-cards/',
+    exportURL: 'C:\\Users\\tlgde\\Desktop\\'
+}
---
+++ index.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+let URL = require('./config')
+let crawlURL = URL.crawlURL;
+let exportURL = URL.exportURL;
+
+(async() => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(crawlURL, { waitUntil: 'networkidle0' });
+
+    const title = await page.evaluate(() => {
+        t = document.getElementsByClassName('UIHeading UIHeading--one')[0].innerText
+        return t;
+    })
+
+    const getQuizzes = await page.evaluate(() => {
+        let quizzes = document.querySelectorAll('div.SetPageTerm-content');
+        quizzes = [...quizzes];
+        let quiz = quizzes.map(e => ({
+            quiz: e.childNodes
+        }))
+        quiz = [...quiz]
+        let questions = quiz.map(e => (e.quiz[0].innerText))
+        let answers = quiz.map(e => (e.quiz[1].innerText))
+        let textQuiz = {}
+        for (let i = 0; i < questions.length; i++) {
+            textQuiz[questions[i]] = answers[i];
+        }
+        return textQuiz;
+    });
+    try {
+        if (fs.existsSync(exportURL + title)) {
+            console.log('Folder existed')
+        } else {
+            await fs.mkdir(exportURL + title, err => { console.log('create folder error: ' + err); return; });
+            await fs.writeFile(exportURL + title + '\\quizbank.txt', JSON.stringify(getQuizzes, null, 2), err => { console.log('write question error: ' + err); return; })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    await browser.close();
+})();
